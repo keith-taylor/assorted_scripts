90aaf0482e51cebcf38314f0f2d70eae8a787923
Keith Taylor <k-taylor@outlook.com>
2023-08-31 17:02:46 +0100

    tidying directory_listing.py

--- directory_listings_with_rich_columns/directory_listing.py
+++ directory_listings_with_rich_columns/directory_listing.py
@@ -6,8 +6,6 @@ from rich.console import Console
 from rich.panel import Panel
 import pathlib
 
-folder_colour = 'yellow'
-file_colour = 'red'
 directory_path = os.path.abspath(sys.argv[1])
 
 def get_directory_contents(directory):
@@ -17,14 +15,13 @@ def get_directory_contents(directory):
             if item[0] == '.':  # remove hidden files
                 continue
             elif os.path.isfile(os.path.join(directory, item)):
-                if item[-3:] == '.py':
+                if item[-3:] == '.py' or item[-4] == '.pyc':
                     file_type = 'file (.py)'
                 else:
                     file_type = 'file'
                 temp_dic = {'name': str(item), 'type': str(file_type)}
                 dir_contents.append(temp_dic)
             else:
-                pass
                 temp_dic = {'name': str(item), 'type': 'folder'}
                 dir_contents.append(temp_dic)
     except FileNotFoundError:
@@ -44,13 +41,10 @@ def get_content(item):
     elif object_type == 'file (.py)':
         return f"🐍 [b][grey]{object_type}[/b]\n{object_name}"
     else:
-        # print(f"From get_content: [b]{object_name}[/b]\n[red]{object_type}")
         return f"📂 [yellow][b]{object_type}[/b]\n{object_name}"
 
 
 console = Console()
 directory_contents = get_directory_contents(directory_path)
-# for i in range(0, len(directory_contents)):
 dir_renderables = [Panel(get_content(item), expand=True) for item in directory_contents]
-
 console.print(Columns(dir_renderables))
